74fe57fdd2b7f02b7ece9885a596697019cb8107
Kamyar <139503563+GlobularOne@users.noreply.github.com>
2024-06-24 15:41:02 +0330

    Multiple files: Add scripts for docker

---
+++ tools/docker_build.py
@@ -0,0 +1,11 @@
+#!/bin/env python3
+
+import os
+
+
+def main():
+    os.system("docker build -t globularone/snr .")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tools/docker_run.py
@@ -0,0 +1,11 @@
+#!/bin/env python3
+
+import os
+
+
+def main():
+    os.system("docker compose run --rm globularone/snr")
+
+
+if __name__ == "__main__":
+    main()
