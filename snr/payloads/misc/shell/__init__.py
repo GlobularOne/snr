08bd5e8b614f105ad748fe2a5bd676bfa50d1f7b
Kamyar <139503563+GlobularOne@users.noreply.github.com>
2024-07-15 12:47:40 +0330

    misc/shell: Minor improvements

--- snr/payloads/misc/shell/__init__.py
+++ snr/payloads/misc/shell/__init__.py
@@ -2,11 +2,12 @@
 Drop to a shell as root
 """
 
-from snr.core.payload.payload import Payload, Context
+from snr.core.payload.payload import Context, Payload
 
 
 class ShellPayload(Payload):
     AUTHORS = ("GlobularOne",)
+    TARGET_OS_LIST = ("Any",)
 
     def generate(self, _: Context) -> int:
         # Do nothing, really nothing
