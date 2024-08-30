ca4b4b3ebc6ff32d39e0c3696cdfcc4caf943883
Kamyar <139503563+GlobularOne@users.noreply.github.com>
2024-08-30 18:21:00 +0330

    Use new rootfs version in bypass_init.sh

--- tools/bypass_init.sh
+++ tools/bypass_init.sh
@@ -4,5 +4,5 @@ mkdir -p ~/.local/share/snr/rootfs
 mkdir -p ~/.config/snr
 mkdir -p ~/.local/state/snr
 mkdir -p ~/.cache/snr
-tar -caf ~/.local/share/snr/jammy-x86_64.tar.gz -C ~/.local/share/snr/rootfs .
-echo -n 2 >  ~/.local/share/snr/jammy-x86_64.tar.gz.version
+tar -caf ~/.local/share/snr/noble-x86_64.tar.gz -C ~/.local/share/snr/rootfs .
+echo -n 3 >  ~/.local/share/snr/noble-x86_64.tar.gz.version
