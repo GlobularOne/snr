5b4694f0684d7c19ad05bdfe78bf57975bbfc37e
Kamyar <139503563+GlobularOne@users.noreply.github.com>
2024-12-06 12:25:00 +0330

    bypass_init.sh: Listen to XDG env vars

--- tools/bypass_init.sh
+++ tools/bypass_init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir -p ~/.local/share/snr/rootfs
-mkdir -p ~/.config/snr
-mkdir -p ~/.local/state/snr
-mkdir -p ~/.cache/snr
-tar -caf ~/.local/share/snr/noble-x86_64.tar.gz -C ~/.local/share/snr/rootfs .
-echo -n 3 >  ~/.local/share/snr/noble-x86_64.tar.gz.version
+mkdir -p ${XDG_DATA_HOME:-~/.local/share}/snr/rootfs
+mkdir -p ${XDG_CONFIG_HOME:-~/.config}/.config/snr
+mkdir -p ${XDG_DATA_HOME:-~/.local/state}/snr
+mkdir -p ${XDG_CACHE_HOME:-~/.cache}/snr
+tar -caf ${XDG_DATA_HOME:-~/.local/share}/snr/noble-x86_64.tar.gz -C ${XDG_DATA_HOME:-~/.local/share}/snr/rootfs .
+echo -n 3 > ${XDG_DATA_HOME:-~/.local/share}/snr/noble-x86_64.tar.gz.version
